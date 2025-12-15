051ce7bb5e7bfaaa60753f1855fe97eed6389b0a
NANORANK <153913035+NANORANK@users.noreply.github.com>
2025-12-15 22:20:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-
 require("dotenv").config();
 
 module.exports = {
   TOKEN: process.env.TOKEN,
-  CLIENT_ID: process.env.CLIENT_ID
+  CLIENT_ID: process.env.CLIENT_ID,
+  TIMEZONE: process.env.TIMEZONE || "Asia/Bangkok"
 };
